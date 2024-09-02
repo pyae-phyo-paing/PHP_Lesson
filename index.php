67a8796e46e8605e6d9902c65e458bf7fbcfc4b5
pyae-phyo-paing <paing.mdy.edu@gmail.com>
2024-09-02 21:34:36 +0630

    php.lesson.intro.2.september2

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php 
 
     echo "Hello World!";
-    echo "<h1>Super Friday</h1>"
-
-
+    echo "<h1>Super Friday</h1>";
+    $language_type = "Server-side programming language";
+    echo $language_type;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+    echo "<h1> This is paragraph </h1>";
+
+    ?>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt minus neque culpa nostrum recusandae eligendi tempora, aut ipsa incidunt similique quas voluptas? Facilis, animi vel?Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nemo magni molestias suscipit vitae cupiditate, quis tenetur possimus delectus. Commodi mollitia aut recusandae officia magni accusamus voluptatem natus dolor fuga.
+    </p>
+
+    <script>
+        document.write('<h1>I am JavaScript!</h1>')
+    </script>
+</body>
+</html>
---
+++ variable_and_string.php
@@ -0,0 +1,15 @@
+<?php
+// Variable
+// Naming Rule - No Space, No Spacial Characters!@#$%^&*(), Can't be a number on first character, Case Sensitive
+
+$name = "Pyae Phyo Paing";
+$address = "Mandalay";
+
+// Double qoute and Single quote
+echo "My name is $name";
+echo '<br>';
+echo 'I live in $address<br>';
+echo 'I live in'.$address;
+echo '<br>';
+echo $name.' live in '.$address;
+?>
