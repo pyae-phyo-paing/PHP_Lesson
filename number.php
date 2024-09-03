5399310032b8d5665afaa394f4a669096ab43952
pyae-phyo-paing <paing.mdy.edu@gmail.com>
2024-09-03 21:46:20 +0630

    sep2.PHP Lessons

---
+++ number.php
@@ -0,0 +1,18 @@
+<?php 
+
+$number1 = 300;
+$number2 = 500;
+
+$number1++; //$number1 + 1
+echo $number1;
+echo "<br>";
+echo $number1++;
+echo "<br>";
+echo $number1;
+echo "<br>";
+echo ++$number2; //$number2 + 1;
+echo "<br>";
+
+$number1 += $number2; //$number1 = $number1 + $number2
+echo $number1;
+?>
--- variable_and_string.php
+++ variable_and_string.php
@@ -12,4 +12,45 @@ echo 'I live in $address<br>';
 echo 'I live in'.$address;
 echo '<br>';
 echo $name.' live in '.$address;
+
+// .=
+$first_name = 'Alan';
+echo "<p>$first_name</p>";
+$last_name = 'Walker';
+echo "<p>$last_name</p>";
+
+$first_name .= 'Walker'; // add another words
+echo "<p>$first_name</p>";
+
+// trim() => remove first space and last space
+$singer = ' Taylar Swift ';
+echo "<p>My favrious singer is$singer.</p>";
+echo "<p>My favrious singer is".trim($singer).".</p>";
+
+// strlen() => string length
+echo strlen($singer);
+echo "<br>";
+$singer_length = trim($singer);
+echo strlen($singer_length);
+echo "<br>";
+
+// strtolower(string) => change to lowercase
+echo strtolower($singer);
+echo "<br>";
+
+// strtoupper(string) => change to uppercase
+echo strtoupper($singer);
+echo "<br>";
+
+// substr(string,start(0),number) => substring (to cut and take the word)
+echo substr("Hello HeinLearner",6);
+echo "<br>";
+echo substr("Hello HeinLearner",6,4);
+echo "<br>";
+echo substr("Hello HeinLearner",10,5);
+echo "<br>";
+// str_replace(search, replace, subject)
+$city = 'Hello Mandalay';
+echo "<p>$city</p>";
+echo str_replace("Mandalay","Smart City",$city);
 ?>
