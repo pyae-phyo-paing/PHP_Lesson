2e3fc25a7bedd33745754b820e98026b8fb75312
pyae-phyo-paing <paing.mdy.edu@gmail.com>
2024-09-05 21:33:04 +0630

    sep5.phplesson

---
+++ date.php
@@ -0,0 +1,29 @@
+<?php 
+    
+    echo "<h1> Date </h1>";
+    echo "<h3>Today is".date("Y-m-d")."</h3>";
+    echo "<h3>Today is".date("d.m.Y")."</h3>";
+    echo "<h3>Today is".date("Y/m/d")."</h3>";
+    echo "<h3>Today is".date("d-m-y")."</h3>";
+    echo "<h3>Today is".date("M d, Y")."</h3>";
+    echo "<h3?Today is".date("l")."</h3>";
+
+    echo "<h1>Time </h1>";
+    echo "<h3>The time is ".date("h:i:sa")."</h3>";
+
+    echo date_default_timezone_get();
+
+    date_default_timezone_set("Asia/Yangon");
+
+    echo "<h3>The Myanmar time is ".date("h:i:sa")."</h3>";
+
+    echo "<h1>String to time</h1>";
+    $today = strtotime("today");
+    echo $today;
+    echo "<h3>Today is ".date("d.m.Y",$today)."</h3>";
+    $tomorrow = strtotime("tomorrow");
+    echo "<h3>Tomorrow is".date("d.m.Y",$tomorrow)."</h3>";
+    echo "<h3>Next Sunday is".date("d.m.Y",strtotime("next Sunday"))."</h3>";
+    echo "<h3>Next 1 week is".date("d.m.Y",strtotime("+1 weeks"))."</h3>";
+    echo "<h3>Next 3 Months is".date("d.m.Y",strtotime("+3 months"))."</h3>";
+?>
--- number.php
+++ number.php
@@ -15,4 +15,17 @@ echo "<br>";
 
 $number1 += $number2; //$number1 = $number1 + $number2
 echo $number1;
+
+$firstNumber = 600;
+$secondNumber = 400;
+$add = $firstNumber + $secondNumber;
+echo "<h3>$add</h3>";
+$sub = $firstNumber - $secondNumber;
+echo "<h3>$sub</h3>";
+$mul = $firstNumber * $secondNumber;
+echo "<h3>$mul</h3>";
+$div = $firstNumber / $secondNumber;
+echo "<h3>$div</h3>";
+$mod = $firstNumber % $secondNumber;
+echo "<h3>$mod</h3>";
 ?>
